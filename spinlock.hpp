7413896f7876f2a88c6ce5761f4eb32f7e8b5768
WASPP <waspp.org@gmail.com>
2015-02-12 23:17:00 +0900

    added spinlock

---
+++ spinlock.hpp
@@ -0,0 +1,54 @@
+//
+// Copyright (c) 2003-2012 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+//
+// Copyright (c) 2014-2015 WASPP (waspp.org@gmail.com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef WASPP_SPINLOCK_HPP
+#define WASPP_SPINLOCK_HPP
+
+#include <boost/atomic.hpp>
+#include <boost/thread.hpp>
+
+#include "spinlock.hpp"
+
+namespace waspp
+{
+
+	class spinlock
+	{
+	public:
+		void acquire()
+		{
+			while (true)
+			{
+				if (!atomic_flag_.test_and_set(boost::memory_order_acquire))
+				{
+					return;
+				}
+
+				boost::this_thread::sleep(boost::posix_time::milliseconds(250));
+			}
+		}
+
+		void release()
+		{
+			atomic_flag_.clear(boost::memory_order_release);
+		}
+
+	private:
+		boost::atomic_flag atomic_flag_;
+
+	};
+
+} // namespace waspp
+
+#endif // WASPP_SPINLOCK_HPP
