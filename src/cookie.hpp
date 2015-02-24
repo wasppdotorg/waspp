6864a98a94b528b2212037c1eb1edadcd080c77b
WASPP <waspp.org@gmail.com>
2015-02-24 13:42:12 +0900

    cookie and session

---
+++ src/cookie.hpp
@@ -0,0 +1,26 @@
+//
+// Copyright (c) 2014-2015 WASPP (waspp.org@gmail.com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef WASPP_COOKIE_HPP
+#define WASPP_COOKIE_HPP
+
+namespace waspp
+{
+
+	class cookie
+	{
+	public:
+		cookie();
+		~cookie();
+
+	private:
+
+	};
+
+} // namespace waspp
+
+#endif // WASPP_COOKIE_HPP
---
+++ src/session.hpp
@@ -0,0 +1,26 @@
+//
+// Copyright (c) 2014-2015 WASPP (waspp.org@gmail.com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef WASPP_SESSION_HPP
+#define WASPP_SESSION_HPP
+
+namespace waspp
+{
+
+	class session
+	{
+	public:
+		session();
+		~session();
+
+	private:
+
+	};
+
+} // namespace waspp
+
+#endif // WASPP_SESSION_HPP
