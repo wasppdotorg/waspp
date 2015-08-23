6d43bf4ad4c06d5b4c8f8276752d526933d7f92c
MobileGameServer <mobilegameserver@gmail.com>
2015-08-23 11:00:37 +0900

    js lib for view updated

---
+++ doc/_.js
@@ -0,0 +1,90 @@
+var _str = function(_elem, _value)
+{
+	if (_elem.tagName == 'A')
+	{
+	}
+	else if (_elem.tagName == 'INPUT')
+	{
+	}
+	else
+	{
+		while (_elem.hasChildNodes())
+		{
+			_elem.removeChild(_elem.childNodes[0]);
+		}
+		
+		_elem.appendChild(document.createTextNode(decodeURIComponent(_value)));
+	}
+};
+
+var _arr = function(_elem, _item)
+{
+	if (_item.length == 0)
+	{
+		return;
+	}
+
+	for (var _key in _item[0])
+	{
+		var _elems = _elem.getElementsByClassName(_key);
+		for (var i = 0; i < _elems.length; ++i)
+		{
+			_str(_elems[i], _item[i][_key]);
+		}
+	}
+};
+
+var _map = function(_elem, _item)
+{	
+	for (var _key in _item)
+	{
+		var _elems = _elem.getElementsByClassName(_key);
+		for (var i = 0; i < _elems[i]; ++i)
+		{
+			_str(_elems[i], _item[i][_key]);
+		}
+	}
+};
+
+var _ = function(_json)
+{
+	for (var _key in _json)
+	{
+		if (!_json[_key])
+		{
+			continue;
+		}
+		else if (_key == '_error')
+		{
+			if (_json['_error']['_code'] != '0')
+			{
+				alert(_json['_error']['_message']);
+				return;
+			}
+		}
+		else if (_json[_key].constructor === 'str'.constructor)
+		{
+			var _elems = document.getElementsByClassName(_key);
+			for (var i = 0; i < _elems.length; ++i)
+			{
+				_str(_elems[i], _json[_key]);
+			}
+		}
+		else if (_json[_key].constructor === [].constructor)
+		{
+			var _elems = document.getElementsByClassName(_key);
+			for (var i = 0; i < _elems.length; ++i)
+			{
+				_arr(_elems[i], _json[_key]);
+			}
+		}
+		else if (_json[_key].constructor === {}.constructor)
+		{
+			var _elems = document.getElementsByClassName(_key);
+			for (var i = 0; i < _elems.length; ++i)
+			{
+				_map(_elems[i], _json[_key]);
+			}
+		}
+	}
+};
