0f14d4d5f43724dfa29338435be5ecdea4dc6321
MobileGameServer <mobilegameserver@gmail.com>
2015-11-15 19:01:19 +0900

    trivial

--- src/connection.hpp
+++ src/connection.hpp
@@ -42,18 +42,18 @@ namespace waspp
 
 	private:
 
-		/// Strand to ensure the connection's handlers are not called concurrently.
-		boost::asio::io_service::strand strand_;
-
-		/// Socket for the connection.
-		boost::asio::ip::tcp::socket socket_;
-
 		/// Handle completion of a read operation.
 		void handle_read(const boost::system::error_code& e,
 			std::size_t bytes_transferred);
 
 		/// Handle completion of a write operation.
 		void handle_write(const boost::system::error_code& e);
+
+		/// Strand to ensure the connection's handlers are not called concurrently.
+		boost::asio::io_service::strand strand_;
+
+		/// Socket for the connection.
+		boost::asio::ip::tcp::socket socket_;
 
 		/// The handler used to process the incoming request.
 		request_handler& request_handler_;
@@ -69,6 +69,7 @@ namespace waspp
 
 		/// The response to be sent back to the client.
 		response response_;
+
 	};
 
 	typedef boost::shared_ptr<connection> connection_ptr;
--- src/connection_ssl.hpp
+++ src/connection_ssl.hpp
@@ -45,18 +45,18 @@ namespace waspp
 
 		void handle_handshake(const boost::system::error_code& e);
 
-		/// Strand to ensure the connection_ssl's handlers are not called concurrently.
-		boost::asio::io_service::strand strand_;
-
-		/// Socket for the connection_ssl.
-		boost::asio::ssl::stream<boost::asio::ip::tcp::socket> socket_;
-
 		/// Handle completion of a read operation.
 		void handle_read(const boost::system::error_code& e,
 			std::size_t bytes_transferred);
 
 		/// Handle completion of a write operation.
 		void handle_write(const boost::system::error_code& e);
+
+		/// Strand to ensure the connection_ssl's handlers are not called concurrently.
+		boost::asio::io_service::strand strand_;
+
+		/// Socket for the connection_ssl.
+		boost::asio::ssl::stream<boost::asio::ip::tcp::socket> socket_;
 
 		/// The handler used to process the incoming request.
 		request_handler& request_handler_;
@@ -72,6 +72,7 @@ namespace waspp
 
 		/// The response to be sent back to the client.
 		response response_;
+
 	};
 
 	typedef boost::shared_ptr<connection_ssl> connection_ssl_ptr;
