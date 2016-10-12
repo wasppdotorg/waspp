e828a80e2d1f0abd2d6991ca597d55a1d460e10d
MobileGameServer <mobilegameserver@gmail.com>
2016-10-12 21:35:00 +0900

    sql updated

--- sql/testing.sql
+++ sql/testing.sql
@@ -1,55 +1,7 @@
 
-CREATE DATABASE `testing_waspp_idx` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE DATABASE `testing_waspp_000` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE DATABASE `testing_waspp_001` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE DATABASE `testing_waspp_etc` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-
-USE `testing_waspp_idx`;
-
-	CREATE TABLE `unique_keys` (
-	  `table_id` varchar(32) NOT NULL,
-	  `incr_amount` int(11) unsigned NOT NULL,
-	  `last_key` bigint(20) unsigned NOT NULL,
-	  PRIMARY KEY (`table_id`)
-	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-	LOCK TABLES `unique_keys` WRITE;
-	INSERT INTO `unique_keys` VALUES ('users_idx',1,0);
-	UNLOCK TABLES;
-
-	DELIMITER ;;
-		CREATE DEFINER=`root`@`localhost` PROCEDURE `USP_GET_UNIQUE_KEYS`(IN `in_table_id` VARCHAR(32), IN `in_key_amount` INT(11) unsigned)
-		BEGIN
-
-		DECLARE EXIT HANDLER FOR SQLEXCEPTION ROLLBACK;
-		DECLARE EXIT HANDLER FOR SQLWARNING ROLLBACK;
-
-		START TRANSACTION;
-
-			UPDATE `unique_keys`
-			SET `last_key` = `last_key` + (`incr_amount` * in_key_amount)
-			WHERE `table_id` = in_table_id;
-
-			SELECT `last_key` 
-			FROM `unique_keys`
-			WHERE `table_id` = in_table_id; 
-
-		COMMIT;
-
-		END ;;
-	DELIMITER ;
-	
-	CREATE TABLE `users_idx` (
-	  `userid` bigint(20) unsigned NOT NULL,
-	  `platformtype` int(11) NOT NULL,
-	  `platformid` varchar(64) NOT NULL,
-	  `username` varchar(64) NOT NULL,
-	  `inserttime` datetime NOT NULL,
-	  `updatetime` datetime NOT NULL,
-	  PRIMARY KEY (`userid`),
-	  UNIQUE KEY `platformid_UNIQUE` (`platformid`),
-	  UNIQUE KEY `username_UNIQUE` (`username`)
-	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 USE `testing_waspp_000`;
 
@@ -61,6 +13,7 @@ USE `testing_waspp_000`;
 	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 	LOCK TABLES `unique_keys` WRITE;
+	INSERT INTO `unique_keys` VALUES ('users',2,0);
 	INSERT INTO `unique_keys` VALUES ('characters',2,0);
 	INSERT INTO `unique_keys` VALUES ('char_inven',2,0);
 	UNLOCK TABLES;
@@ -89,10 +42,15 @@ USE `testing_waspp_000`;
 	
 	CREATE TABLE `users` (
 	  `userid` bigint(20) unsigned NOT NULL,
+	  `platformid` varchar(64) NOT NULL,
+	  `username` varchar(64) NOT NULL,
 	  `passwd` varchar(32) NOT NULL,
+	  `platformtype` int(11) NOT NULL,
 	  `inserttime` datetime NOT NULL,
 	  `updatetime` datetime NOT NULL,
-	  PRIMARY KEY (`userid`)
+	  PRIMARY KEY (`userid`),
+	  UNIQUE KEY `platformid_UNIQUE` (`platformid`),
+	  UNIQUE KEY `username_UNIQUE` (`username`)
 	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 	
 	CREATE TABLE `characters` (
@@ -121,6 +79,7 @@ USE `testing_waspp_001`;
 	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 	LOCK TABLES `unique_keys` WRITE;
+	INSERT INTO `unique_keys` VALUES ('users',2,1);
 	INSERT INTO `unique_keys` VALUES ('characters',2,1);
 	INSERT INTO `unique_keys` VALUES ('char_inven',2,1);
 	UNLOCK TABLES;
@@ -149,10 +108,15 @@ USE `testing_waspp_001`;
 	
 	CREATE TABLE `users` (
 	  `userid` bigint(20) unsigned NOT NULL,
+	  `platformid` varchar(64) NOT NULL,
+	  `username` varchar(64) NOT NULL,
 	  `passwd` varchar(32) NOT NULL,
+	  `platformtype` int(11) NOT NULL,
 	  `inserttime` datetime NOT NULL,
 	  `updatetime` datetime NOT NULL,
-	  PRIMARY KEY (`userid`)
+	  PRIMARY KEY (`userid`),
+	  UNIQUE KEY `platformid_UNIQUE` (`platformid`),
+	  UNIQUE KEY `username_UNIQUE` (`username`)
 	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 	
 	CREATE TABLE `characters` (
