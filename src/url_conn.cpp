9c8115123d0859b03f3a2a3ed5c1c71fc0a85850
MobileGameServer <mobilegameserver@gmail.com>
2016-10-12 23:36:13 +0900

    renamed to url_conn

--- src/url_conn.cpp
+++ src/url_conn.cpp
@@ -7,24 +7,24 @@ http://www.boost.org/LICENSE_1_0.txt
 
 #include <iostream>
 
-#include "curl_easy.hpp"
+#include "url_conn.hpp"
 
 namespace waspp
 {
 
-	std::size_t curl_easy_write_func(char* ptr, std::size_t size, std::size_t nmemb, void* userdata)
+	std::size_t curl_easy_write(char* ptr, std::size_t size, std::size_t nmemb, void* userdata)
 	{
 		((std::string*)userdata)->append(ptr, size * nmemb);
 		return size * nmemb;
 	}
 
-	curl_easy::curl_easy(const std::string& url)
+	url_conn::url_conn(const std::string& url)
 		: curl(nullptr), url_(url)
 	{
 		curl = curl_easy_init();
 	}
 
-	curl_easy::~curl_easy()
+	url_conn::~url_conn()
 	{
 		if (curl)
 		{
@@ -32,7 +32,7 @@ namespace waspp
 		}
 	}
 
-	bool curl_easy::perform()
+	bool url_conn::query()
 	{
 		if (!curl)
 		{
@@ -46,7 +46,7 @@ namespace waspp
 			curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
 		}
 		
-		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curl_easy_write_func);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curl_easy_write);
 		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &content_);
 
 		CURLcode code = curl_easy_perform(curl);
--- src/url_conn.hpp
+++ src/url_conn.hpp
@@ -5,8 +5,8 @@ Distributed under the Boost Software License, Version 1.0.
 http://www.boost.org/LICENSE_1_0.txt
 */
 
-#ifndef curl_easy_hpp
-#define curl_easy_hpp
+#ifndef url_conn_hpp
+#define url_conn_hpp
 
 #include <curl/curl.h>
 
@@ -15,17 +15,17 @@ http://www.boost.org/LICENSE_1_0.txt
 namespace waspp
 {
 
-	std::size_t curl_easy_write_func(char* ptr, std::size_t size, std::size_t nmemb, void* userdata);
+	std::size_t curl_easy_write(char* ptr, std::size_t size, std::size_t nmemb, void* userdata);
 
-	class curl_easy
+	class url_conn
 	{
 	public:
-		curl_easy(const std::string& url);
-		~curl_easy();
+		url_conn(const std::string& url);
+		~url_conn();
 
 		const std::string& content() { return content_; }
 
-		bool perform();
+		bool query();
 
 	private:
 		CURL* curl;
@@ -37,4 +37,4 @@ namespace waspp
 
 } // namespace waspp
 
-#endif // curl_easy_hpp
+#endif // url_conn_hpp
