5944e2a9b6cb59671ee3e1c1eb4a383429a2abb8
MobileGameServer <mobilegameserver@gmail.com>
2017-01-06 13:42:19 +0900

    redis3m without boost

--- src/redis3m.cpp
+++ src/redis3m.cpp
@@ -206,253 +206,37 @@ Apache License
 
 */
 
-#ifndef redis3m_hpp
-#define redis3m_hpp
-
-#include <ctime>
-
-#include <hiredis/hiredis.h>
-
-#include <string>
-#include <vector>
-#include <stdexcept>
-
-// code from redis3m
-/*
-redis3m::connection* rdconn = redis3m::connection::connect();
-rdconn->run(redis3m::command("SET") << "foo" << "bar");
-redis3m::reply r = rdconn->run(redis3m::command("GET") << "foo");
-std::cout << "foo is " << r.str() << "\n";
-delete rdconn;
-*/
+#include "redis3m.hpp"
 
 namespace redis3m
 {
 
-	enum type_t
+	template<>
+	command& command::operator<<(const char* arg)
 	{
-		STRING_ = 1,
-		ARRAY_ = 2,
-		INTEGER_ = 3,
-		NIL_ = 4,
-		STATUS_ = 5,
-		ERROR_ = 6
-	};
+		args.push_back(arg);
+		return *this;
+	}
 
-	class reply
+	template<>
+	command& command::operator()(const char* arg)
 	{
-	public:
-		type_t type() const { return type_; }
-		const std::string& str() const { return str_; }
-		int64_t integer() const { return integer_; }
-		const std::vector<reply>& elements() const { return elements_; }
+		args.push_back(arg);
+		return *this;
+	}
 
-		operator const std::string&() const { return str_; }
-		operator const int64_t() const { return integer_; }
-		bool operator==(const std::string& rvalue) const
-		{
-			if (type_ == STRING_ || type_ == ERROR_ || type_ == STATUS_)
-			{
-				return str_ == rvalue;
-			}
-
-			return false;
-		}
-
-		bool operator==(const int64_t rvalue) const
-		{
-			if (type_ == INTEGER_)
-			{
-				return integer_ == rvalue;
-			}
-
-			return false;
-		}
-
-	private:
-		reply(redisReply* rep) : type_(ERROR_), integer_(0)
-		{
-			type_ = static_cast<type_t>(rep->type);
-
-			switch (type_)
-			{
-			case ERROR_:
-			case STRING_:
-			case STATUS_:
-				str_ = std::string(rep->str, rep->len);
-				break;
-			case INTEGER_:
-				integer_ = rep->integer;
-				break;
-			case ARRAY_:
-				for (std::size_t i = 0; i < rep->elements; ++i)
-				{
-					elements_.push_back(reply(rep->element[i]));
-				}
-				break;
-			default:
-				break;
-			}
-		}
-
-		type_t type_;
-		std::string str_;
-		int64_t integer_;
-		std::vector<reply> elements_;
-
-		friend class connection;
-
-	};
-
-	class command
+	template<>
+	command& command::operator<<(const std::string arg)
 	{
-	public:
-		command()
-		{
+		args.push_back(std::move(arg));
+		return *this;
+	}
 
-		}
-
-		command(std::string arg)
-		{
-			args.push_back(std::move(arg));
-		}
-
-		template<typename T>
-		command& operator<<(const T arg)
-		{
-			args.push_back(std::to_string(arg));
-			return *this;
-		}
-
-		template<typename T>
-		command& operator()(const T arg)
-		{
-			args.push_back(std::to_string(arg));
-			return *this;
-		}
-
-		operator const std::vector<std::string>&()
-		{
-			return args;
-		}
-
-	private:
-		std::vector<std::string> args;
-
-	};
-
-	class connection
+	template<>
+	command& command::operator()(const std::string arg)
 	{
-	public:
-		connection(const connection&) = delete;
-		connection& operator=(const connection&) = delete;
-
-		connection(const std::string& host, const unsigned int port, bool pooled_ = true)
-		{
-			timeval timeval_ = { 1, 0 };
-
-			c = redisConnectWithTimeout(host.c_str(), port, timeval_);
-			if (c->err != REDIS_OK)
-			{
-				throw std::runtime_error("redisConnectWithTimeout failed");
-			}
-
-			auto time_ = std::time(nullptr);
-			set_released(*std::localtime(&time_));
-			set_pooled(pooled_);
-		}
-
-		~connection()
-		{
-			redisFree(c);
-		}
-
-		static connection* connect(const std::string& host = "localhost", const unsigned int port = 6379, bool pooled_ = true)
-		{
-			return new connection(host, port, pooled_);
-		}
-
-		bool is_valid() const
-		{
-			return c->err == REDIS_OK;
-		}
-
-		void append(const std::vector<std::string>& args)
-		{
-			std::vector<const char*> argv;
-			argv.reserve(args.size());
-
-			std::vector<std::size_t> argv_lengths;
-			argv_lengths.reserve(args.size());
-
-			std::vector<std::string>::const_iterator i;
-			for (i = args.begin(); i != args.end(); ++i)
-			{
-				argv.push_back(i->c_str());
-				argv_lengths.push_back(i->size());
-			}
-
-			int result = redisAppendCommandArgv(c, static_cast<int>(args.size()), argv.data(), argv_lengths.data());
-			if (result != REDIS_OK)
-			{
-				throw std::runtime_error("redisAppendCommandArgv failed");
-			}
-		}
-
-		reply get_reply()
-		{
-			redisReply* r;
-
-			int result = redisGetReply(c, reinterpret_cast<void**>(&r));
-			if (result != REDIS_OK)
-			{
-				throw std::runtime_error("redisGetReply failed");
-			}
-
-			reply rep(r);
-			freeReplyObject(r);
-
-			if (rep.type() == ERROR_ && (rep.str().find("READONLY") == 0))
-			{
-				throw std::runtime_error("read only");
-			}
-
-			return rep;
-		}
-
-		std::vector<reply> get_replies(unsigned int count)
-		{
-			std::vector<reply> replies;
-			for (unsigned int i = 0; i < count; ++i)
-			{
-				replies.push_back(get_reply());
-			}
-
-			return replies;
-		}
-
-		reply run(const std::vector<std::string>& args)
-		{
-			append(args);
-			return get_reply();
-		}
-
-		redisContext* c_ptr() { return c; }
-
-		std::tm* last_released() { return &released; }
-		void set_released(const std::tm& released_) { released = released_; }
-
-		bool is_pooled() { return pooled; }
-		void set_pooled(bool pooled_) { pooled = pooled_; }
-
-	private:
-		redisContext* c;
-
-		std::tm released;
-		bool pooled;
-
-	};
+		args.push_back(std::move(arg));
+		return *this;
+	}
 
 } // namespace redis3m
-
-#endif // redis3m_hpp
